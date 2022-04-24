8d6417d7a691f86673c4f6a5390a93869f5d14d6
LManhL <95266634+LManhL@users.noreply.github.com>
2022-04-24 17:25:14 +0700

    Update TestOne.cpp

--- TestOne.cpp
+++ TestOne.cpp
@@ -3,9 +3,5 @@ using namespace std;
 int main(){
     cout<<"Hello World!";
     cout<<"Update nay";
-<<<<<<< HEAD
     cout<<"Update tiep oce";
-=======
-    cout<<"Update tiep nay";
->>>>>>> d1dd927a355e2796c454fe8cc2049f93147c3c6a
 }
